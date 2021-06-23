faa884e3e13f0b8ef6307eed4dfc6b7ba2d8039f
Erik Nielsen <erik@312development.com>
2021-06-23 07:44:35 -0500

    updated config and build files

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from pathlib import Path
 
 from setuptools import setup
 
-from trading_scripts import __version__
-
-
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
+
+
 def read(fname="README.md"):
     return open(Path(__file__).parent / fname).read()
 
@@ -19,28 +18,30 @@ def read_requirements(filename):
 
 
 settings = dict(
-    name="alpaca-trading-scripts",
+    # name="alpaca_trading_scripts",
     packages=["trading_scripts"],
-    version=__version__,
-    author="Erik Nielsen",
-    author_email="erik@312development.com",
-    description=("Python trading scripts"),
-    license="MIT",
-    keywords="alpaca",
-    url="https://github.com/nielse63/alpaca-trading-scripts",
+    # version=__version__,
+    # author="Erik Nielsen",
+    # author_email="erik@312development.com",
+    # description=("Python trading scripts"),
+    # license="MIT",
+    # keywords="alpaca",
+    # url="https://github.com/nielse63/alpaca-trading-scripts",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    python_requires=">=3.9",
+    # python_requires=">=3.9",
     install_requires=read_requirements("requirements.txt"),
     # tests_require=read_requirements("requirements-dev.txt"),
-    classifiers=[
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "License :: OSI Approved :: MIT License",
-    ],
+    # classifiers=[
+    #     "Topic :: Software Development :: Libraries :: Python Modules",
+    #     "Programming Language :: Python :: 3.9",
+    #     "Programming Language :: Python :: 3.9",
+    #     "Programming Language :: Python :: Implementation :: PyPy",
+    #     "Topic :: Software Development :: Libraries :: Python Modules",
+    #     "License :: OSI Approved :: MIT License",
+    # ],
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
 )
 
 
