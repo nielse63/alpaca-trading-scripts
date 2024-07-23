2a2b4a2100ec421aae41c4057f71fd624e5d8502
Erik Nielsen <erik@312development.com>
2024-07-23 01:48:20 -0400

    updated run function

--- src/crypto/cron.ts
+++ src/crypto/cron.ts
@@ -1,9 +1,9 @@
-import cron from 'node-cron';
-import run from './run';
 import { log } from '../helpers';
+import run from './run';
 
-cron.schedule('*/15 * * * *', async () => {
+const TIME_INTERVAL = 1000 * 60 * 15; // 15 minutes
+setInterval(async () => {
   log('');
   log(`executing crypto at ${new Date().toISOString()}`);
   await run();
-});
+}, TIME_INTERVAL);
--- src/crypto/run.ts
+++ src/crypto/run.ts
@@ -4,9 +4,9 @@ import { log } from '../helpers';
 import { waitForOrderFill } from '../order';
 import {
   calculateIndicators,
+  CRYPTO_UNIVERSE,
   fetchHistoricalData,
   generateSignals,
-  CRYPTO_SYMBOL,
 } from './helpers';
 
 type BarObject = {
@@ -29,19 +29,40 @@ type BarObjectWithSignals = BarObjectWithIndicators & {
   signal: number;
 };
 
-export async function placeOrder(data: BarObjectWithSignals[]) {
-  log('placing order');
+type AlpacaPosition = {
+  asset_id: string;
+  symbol: string;
+  exchange: string;
+  asset_class: string;
+  asset_marginable: boolean;
+  qty: string;
+  avg_entry_price: string;
+  side: string;
+  market_value: string;
+  cost_basis: string;
+  unrealized_pl: string;
+  unrealized_plpc: string;
+  unrealized_intraday_pl: string;
+  unrealized_intraday_plpc: string;
+  current_price: string;
+  lastday_price: string;
+  change_today: string;
+  qty_available: string;
+};
+
+export async function placeOrder(data: BarObjectWithSignals[], symbol: string) {
+  log(`placing order for ${symbol}`);
   const capital = await getBuyingPower();
   log(`current capital: ${capital}`);
   const lastBar = data[data.length - 1];
-  log(`last bar: ${JSON.stringify(lastBar, null, 2)}`);
+  log(`last bar: ${JSON.stringify({ ...lastBar, symbol }, null, 2)}`);
   // buy signal
   if (lastBar.signal === 1) {
     const qty = parseFloat((capital / lastBar.close).toFixed(0));
-    log(`Placing buy order for ${qty} shares of ${CRYPTO_SYMBOL}`);
+    log(`Placing buy order for ${qty} shares of ${symbol}`);
     if (qty > 0) {
       const buyOrder = await alpaca.createOrder({
-        symbol: CRYPTO_SYMBOL,
+        symbol: symbol,
         qty: qty,
         side: 'buy',
         type: 'market',
@@ -50,7 +71,7 @@ export async function placeOrder(data: BarObjectWithSignals[]) {
       await waitForOrderFill(buyOrder.id);
       log(`buy order placed: ${JSON.stringify(buyOrder, null, 2)}`);
       const tslOrder = await alpaca.createOrder({
-        symbol: CRYPTO_SYMBOL,
+        symbol: symbol,
         qty: buyOrder.qty,
         side: 'sell',
         type: 'trailing_stop',
@@ -62,27 +83,61 @@ export async function placeOrder(data: BarObjectWithSignals[]) {
       );
     }
   } else if (lastBar.signal === -1) {
-    log('should sell');
-    const positions = await alpaca.getPosition(CRYPTO_SYMBOL);
+    log(`should sell ${symbol}`);
+    const positions = await alpaca.getPosition(symbol);
     log(`open positions: ${JSON.stringify(positions, null, 2)}`);
     if (positions) {
       log('cancelling all open orders');
       await alpaca.cancelAllOrders();
-      log('closing all posiitons');
-      await alpaca.closePosition(CRYPTO_SYMBOL);
+      log(`closing all posiitons for ${symbol}`);
+      await alpaca.closePosition(symbol);
     }
   }
+  console.log('');
 }
 
-const run = async () => {
-  const data = await fetchHistoricalData(CRYPTO_SYMBOL);
+const runForSymbol = async (symbol: string) => {
+  const data = await fetchHistoricalData(symbol);
   const dataWithIndicators = calculateIndicators(data);
   const dataWithSignals = generateSignals(dataWithIndicators);
   try {
-    await placeOrder(dataWithSignals);
+    await placeOrder(dataWithSignals, symbol);
   } catch (error) {
     log(`Error placing order: ${error}`);
   }
 };
+
+const run = async () => {
+  const positions = await alpaca.getPositions();
+  const crypotPositions = positions
+    .filter((position: AlpacaPosition) => {
+      return position.asset_class === 'crypto';
+    })
+    .map((position: AlpacaPosition) => {
+      return {
+        ...position,
+        symbol: position.symbol.replace('USD', '/USD'),
+      };
+    });
+
+  // determine if we need to sell open positions
+  for (const position of crypotPositions) {
+    const { symbol } = position;
+    const data = await fetchHistoricalData(symbol);
+    const dataWithIndicators = calculateIndicators(data);
+    const dataWithSignals = generateSignals(dataWithIndicators);
+    const lastBar = dataWithSignals[data.length - 1];
+    if (lastBar.signal === -1) {
+      log(`closing all posiitons for ${symbol}`);
+      await alpaca.closePosition(symbol);
+    }
+  }
+
+  for (const symbol of CRYPTO_UNIVERSE) {
+    await runForSymbol(symbol);
+  }
+};
+
+// run().catch(console.error);
 
 export default run;
